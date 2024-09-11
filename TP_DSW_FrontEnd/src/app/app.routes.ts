550b32abe43ada05707eba503e625ce0c1461674
luciomondelli <luciomondelli123@gmail.com>
2024-09-11 17:34:20 -0300

    Agrego bootstrap y fontawesome, sumo primer login

--- TP_DSW_FrontEnd/src/app/app.routes.ts
+++ TP_DSW_FrontEnd/src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { LayoutComponent } from './pages/layout/layout.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        redirectTo:'Login',
+        pathMatch: 'full'
+    },
+    {
+        path:'Login',
+        component: LoginComponent
+    },
+    {
+        path:'',
+        component:LayoutComponent,
+        children: [
+            {
+                path:'Dashboard',
+                component:DashboardComponent
+            }
+        ]
+    }
+];
